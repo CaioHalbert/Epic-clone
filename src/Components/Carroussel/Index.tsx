5cc2e232b417cd806f64c12f07939a140a122106
Caio Halbert <caio.Halbert@gmail.com>
2024-03-15 11:54:05 -0300

    spic card update

---
+++ src/Components/Carroussel/Index.tsx
@@ -0,0 +1,17 @@
+import Card from "../MiniRef/Index";
+import './Style.css'
+
+const Carroussel = () => {
+    return(
+        <div className="cr">
+            <div className="imgGrandona">
+
+            </div>
+            <div className="vComp">
+                <Card/>
+            </div>
+        </div>
+    )
+}
+
+export default Carroussel;
---
+++ src/Components/MiniRef/Index.tsx
@@ -0,0 +1,12 @@
+import './Style.css';
+
+const Card = () => {
+    return(
+        <div className="card">
+            <img id="cardImg" src="https://pbs.twimg.com/media/E-4lJkzVkAEXtBr.jpg"/>
+            <h1 id="cardTxt">texto aqui</h1>
+        </div>
+    )
+}
+
+export default Card;
